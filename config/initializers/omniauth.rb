159a0fe4870ed419c27ac365ba3a66c0e8615b89
Ben <ben@dee.mx>
2022-06-13 11:23:31 +0200

    oauth scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :google_oauth2,
     Rails.application.credentials.google_client_id,
-    Rails.application.credentials.google_client_secret
+    Rails.application.credentials.google_client_secret,
+    {
+      scope: 'userinfo.email,userinfo.profile,openid,https://www.googleapis.com/auth/drive.file',
+      prompt: 'select_account',
+      image_aspect_ratio: 'square',
+      image_size: 50
+    }
   )
 end
 
